224c86de43f7f58b6bd1a7b5c309ac3ba9edc9c5
BrandonVB11 <brandon.vanbellinghen@student.vinci.be>
2024-04-22 11:33:13 +0200

    feat: add inscription part for server and client

--- makefile
+++ makefile
@@ -5,13 +5,13 @@ all: $(ALL)
 server : server.o utils_v1.o
 	$(CC) $(CCFLAGS) -o server server.o utils_v1.o
 	
-server.o: server.c messages.h
+server.o: server.c message.h
 	$(CC) $(CCFLAGS) -c server.c
 
 client : client.o utils_v1.o
 	$(CC) $(CCFLAGS) -o client client.o utils_v1.o
 
-client.o: client.c messages.h
+client.o: client.c message.h
 	$(CC) $(CCFLAGS) -c client.c
 
 utils_v1.o: utils_v1.c utils_v1.h
---
+++ server.c
@@ -0,0 +1,140 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "message.h"
+#include "utils_v1.h"
+
+#define MIN_PLAYERS 2
+#define BACKLOG 5
+#define TIME_INSCRIPTION 30
+
+typedef struct Player
+{
+	char pseudo[MAX_PSEUDO];
+	int sockfd;
+	int position_tuile;
+    int score;
+} Player;
+
+Player tabPlayers[MIN_PLAYERS];
+volatile sig_atomic_t end_inscriptions = 0;
+
+void endServerHandler(int sig)
+{
+	end_inscriptions = 1;
+}
+
+void disconnect_players(Player *tabPlayers, int nbPlayers)
+{
+	for (int i = 0; i < nbPlayers; i++)
+		sclose(tabPlayers[i].sockfd);
+	return;
+}
+
+/**
+ * PRE:  serverPort: a valid port number
+ * POST: on success, binds a socket to 0.0.0.0:serverPort and listens to it ;
+ *       on failure, displays error cause and quits the program
+ * RES:  return socket file descriptor
+ */
+int setup_server_socket(int port){
+    int sockfd = ssocket();
+
+	int option = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int));
+
+	sbind(port, sockfd);
+
+	slisten(sockfd, BACKLOG);
+
+    return sockfd;
+}
+
+/*int accept_client_connection(int server_socket){
+
+}*/
+
+
+int main(int argc, char const *argv[])
+{
+    int sockfd, newsockfd, i;
+	StructMessage msg;
+	int ret;
+	struct pollfd fds[MIN_PLAYERS];
+    
+    ssigaction(SIGALRM, endServerHandler);
+
+	sockfd = setup_server_socket(SERVER_PORT);
+	printf("Le serveur tourne sur le port : %i \n", SERVER_PORT);
+
+	i = 0;
+	int nbPLayers = 0;
+
+	// INSCRIPTION PART
+	alarm(TIME_INSCRIPTION);
+
+	while (!end_inscriptions)
+	{
+		/* client trt */
+		newsockfd = accept(sockfd, NULL, NULL); 
+		if (newsockfd > 0)						
+		{
+
+			ret = sread(newsockfd, &msg, sizeof(msg));
+
+			if (msg.code == INSCRIPTION_REQUEST)
+			{
+				printf("Inscription demandée par le joueur : %s\n", msg.messageText);
+
+				strcpy(tabPlayers[i].pseudo, msg.messageText);
+				tabPlayers[i].sockfd = newsockfd;
+				i++;
+
+				if (nbPLayers < MIN_PLAYERS)
+				{
+					msg.code = INSCRIPTION_OK;
+					nbPLayers++;
+					if (nbPLayers == MIN_PLAYERS)
+					{
+						alarm(0);
+						end_inscriptions = 1;
+					}
+				}
+				else
+				{
+					msg.code = INSCRIPTION_KO;
+				}
+				ret = swrite(newsockfd, &msg, sizeof(msg));
+				printf("Nb Inscriptions : %i\n", nbPLayers);
+			}
+		}
+	}
+
+	printf("FIN DES INSCRIPTIONS\n");
+	if (nbPLayers < MIN_PLAYERS)
+	{
+		printf("PARTIE ANNULEE .. PAS ASSEZ DE JOUEURS\n");
+		msg.code = CANCEL_GAME;
+		for (i = 0; i < nbPLayers; i++)
+		{
+			swrite(tabPlayers[i].sockfd, &msg, sizeof(msg));
+		}
+		disconnect_players(tabPlayers, nbPLayers);
+		sclose(sockfd);
+		exit(0);
+	}
+	else
+	{
+		printf("PARTIE VA DEMARRER ... \n");
+		msg.code = START_GAME;
+		for (i = 0; i < nbPLayers; i++)
+			swrite(tabPlayers[i].sockfd, &msg, sizeof(msg));
+	}
+}
